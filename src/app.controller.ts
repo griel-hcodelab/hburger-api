d1dbdb3d8921a8e529e3df146f95838f817a235c
robertogriel <roberto@griel.com.br>
2022-03-02 10:35:47 -0300

    Primeiro Commit, início do projeto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,12 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	constructor(private readonly appService: AppService) { }
+
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
+	
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Seja bem-vindo à API do Projeto Hburger.';
   }
+
 }
