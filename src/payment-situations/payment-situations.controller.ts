faf87f0cab4ab6c7b51f5ac05e0ef124576e6200
Guilherme-Ferreti <guiferretis@gmail.com>
2022-03-04 20:30:30 -0300

    Adicionado criação de payment situation

--- src/payment-situations/payment-situations.controller.ts
+++ src/payment-situations/payment-situations.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreatePaymentSituationDto } from './dto/create-payment-situation.dto';
 import { PaymentSituationsService } from './payment-situations.service';
 
 @Controller('payment-situations')
@@ -8,5 +9,10 @@ export class PaymentSituationsController {
     @Get('/')
     async findAll() {
         return this.paymentSituationsService.findAll();
+    }
+
+    @Post('/')
+    async create(@Body() data: CreatePaymentSituationDto) {
+        return this.paymentSituationsService.create(data);
     }
 }
--- src/payment-situations/payment-situations.service.ts
+++ src/payment-situations/payment-situations.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePaymentSituationDto } from './dto/create-payment-situation.dto';
 
 @Injectable()
 export class PaymentSituationsService {
@@ -7,5 +8,11 @@ export class PaymentSituationsService {
 
     async findAll() {
         return this.prisma.paymentSituation.findMany();
+    }
+
+    async create(data: CreatePaymentSituationDto) {
+        return this.prisma.paymentSituation.create({
+            data
+        });
     }
 }
