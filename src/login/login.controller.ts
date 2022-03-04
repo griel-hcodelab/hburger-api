565ad30e813fbaf3486659befecf2a0ad910515f
robertogriel <roberto@griel.com.br>
2022-03-04 09:47:34 -0300

    Função para checar a senha e as datas

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -2,61 +2,27 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException 
 import { LoginService } from './login.service';
 import { CreateLoginDto } from './dto/create-login.dto';
 import { UpdateLoginDto } from './dto/update-login.dto';
-import { parse } from 'date-fns';
+import { checkDate } from 'utils/checkDate';
+
 
 @Controller('login')
 export class LoginController {
 	constructor(private readonly loginService: LoginService) { }
 
-	checkDate(date: string)
-	{
-		const splited = date.split('-')
-		
-		for (let i = 0; i < splited.length; i++) {
-
-			if (splited[0] < '1900' || splited[0] > new Date().getFullYear().toString()) {
-				throw new BadRequestException("O ano de nascimento é inválido")
-			}
-
-			if (splited[1] < '1' || splited[1] > '12') {
-				throw new BadRequestException("O mês de nascimento é inválido")
-			}
-
-			if (splited[2] < '1' || splited[2] > '31') {
-				throw new BadRequestException("O dia de nascimento é inválido")
-			}
-
-			const n = Number(splited[i]);
-
-			if (isNaN(n)) {
-				throw new BadRequestException("Esta data de nascimento é inválida")
-			}
-		}
-
-		console.log(parse(date, 'yyyy-MM-dd', new Date()))
-	}
+	
 
 	@Post()
 	create(@Body() body: CreateLoginDto) {
 
-		// if (body.birthAt) {
-        //     const birthAt = parse(body.birthAt, 'yyyy-MM-dd', new Date());
-		// 	Object.assign(body, {birthAt})
-
-		// 	console.log({birthAt})
-
-		// 	if (birthAt instanceof Date) {
-		// 		console.log('é instancia')
-		// 	}
-
-        // }
-
 		if (body.birthAt) {
-			this.checkDate(body.birthAt)
+			Object.assign(body, checkDate(body.birthAt))
 		}
 
+		if (body.password && body.password.length < 6) {
+			throw new BadRequestException("A sua senha é muito curta. Escolha uma senha mais segura.");
+		}
 
-		//return this.loginService.create(body);
+		return this.loginService.create(body);
 		
 	}
 
--- utils/checkDate.ts
+++ utils/checkDate.ts
@@ -1,30 +1,14 @@
 import { BadRequestException } from '@nestjs/common';
-import { parse } from 'date-fns';
+import { parse, isValid } from 'date-fns';
 
-export function checkDate(date: string)
-	{
-		const splited = date.split('-')
-		
-		for (let i = 0; i < splited.length; i++) {
+export function checkDate(date: string) {
+    
+    const parsedDate = parse(date, 'yyyy-MM-dd', new Date());
 
-			if (splited[0] < '1900' || splited[0] > new Date().getFullYear().toString()) {
-				throw new BadRequestException("O ano de nascimento é inválido")
-			}
+    if (!isValid(parsedDate)) {
+        throw new BadRequestException("Esta data de nascimento está incorreta.");
+    } else {
+        return parsedDate;
+    }
 
-			if (splited[1] < '1' || splited[1] > '12') {
-				throw new BadRequestException("O mês de nascimento é inválido")
-			}
-
-			if (splited[2] < '1' || splited[2] > '31') {
-				throw new BadRequestException("O dia de nascimento é inválido")
-			}
-
-			const n = Number(splited[i]);
-
-			if (isNaN(n)) {
-				throw new BadRequestException("Esta data de nascimento é inválida")
-			}
-		}
-
-		return parse(date, 'yyyy-MM-dd', new Date())
-	}
+}
