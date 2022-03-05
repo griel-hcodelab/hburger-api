dabaee33d602643181a9b8efadef5317679055d1
Pedro Bernardinelli <pbernardinelli@gmail.com>
2022-03-04 21:25:17 -0300

    Products

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-        PrismaModule, LoginModule],
+        PrismaModule, LoginModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
