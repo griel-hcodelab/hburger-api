0ec5e5bbe4187fc2a44ad0bc1706453e15c9325d
robertogriel <roberto@griel.com.br>
2022-03-09 20:51:45 -0300

    Alteração de mensagem de erro

--- utils/checkNumber.ts
+++ utils/checkNumber.ts
@@ -1,16 +1,22 @@
 import { BadRequestException } from "@nestjs/common";
 
-export function checkNumber(number:any):number
+export function checkNumber(number:any, message:string = ''):number
 {
 
+    let error:string = "Este código ou ID é incorreto. Por favor, forneça o código correto.";
+
+    if (message) {
+        error = message;
+    }
+
     if (!number) {
-        throw new BadRequestException("Por favor, informe o código ou o ID.");
+        throw new BadRequestException("Por favor, informe o valor correspondente ao campo.");
     }
 
     number = Number(number);
 
     if (isNaN(number)) {
-        throw new BadRequestException("Este código ou ID é incorreto. Por favor, forneça o código correto.");
+        throw new BadRequestException(error);
     }
 
     return number;
